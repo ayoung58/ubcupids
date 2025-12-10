23506941fae41cdb43e549e8a16648ad47d4c4de
Alvin Young <yalvin58@gmail.com>
2025-12-10 12:39:58 -0800

    Add shadcn UI package and basic components

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,4 @@
-// Merge Tailwind CSS classes intelligently (used by shadcn/ui components)
-
-import { type ClassValue, clsx } from "clsx"
+import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
